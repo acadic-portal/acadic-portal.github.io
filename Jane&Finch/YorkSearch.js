ea41f5665a5f6a8e05e93d608f43b7b1ba5164db
acadic-portal <122941412+acadic-portal@users.noreply.github.com>
2023-05-18 12:02:34 -0400

    Add files via upload

---
+++ Jane&Finch/YorkSearch.js
@@ -0,0 +1,14 @@
+searchkeywords = [
+{"name": "All Urgency Emergency Deparment Visits in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/all-emergency-department-visits-in-toronto/", "keywords": ["emergency", "department", "ed", "urgent", "all", "urgency"]},
+{"name": "Asthma in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/asthma-in-toronto/", "keywords": ["preventive", "disease", "asthma", "respiratory", "breath", "genetic", "lung", "corticosteroids"]},
+{"name": "COPD in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/copd-in-toronto/", "keywords": ["preventive", "disease", "copd", "chronic", "obstructive", "pulmonary", "smoke", "respiratory", "cough", "breath", "lung"]},
+{"name": "Diabetes in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/diabetes-in-toronto/", "keywords": ["preventive", "disease", "chronic", "diabetes", "insulin", "blood", "sugar", "beta"]},
+{"name": "High-Urgency Emergency Deparment Visits in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/high-urgency-emergency-department-visits-in-toronto/", "keywords": ["emergency", "department", "ed", "urgent", "high", "urgency"]},
+{"name": "Hypertension in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/hypertension-in-toronto/", "keywords": ["preventive", "disease", "hypertension", "hbp", "high", "blood", "pressure", "genetic", "smoke", "alcohol"]},
+{"name": "Low-Urgency Emergency Deparment Visits in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/low-urgency-emergency-department-visits-in-toronto/", "keywords": ["emergency", "department", "ed", "urgent", "low", "urgency"]},
+{"name": "Mammogram Tests in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/mammogram-tests-in-toronto/", "keywords": ["women", "tests", "screen", "mammogram", "mammography", "breast", "cancer"]},
+{"name": "Mental Health in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/mental-health-in-toronto/", "keywords": ["preventive", "disease", "mental", "health", "disorder", "depression", "drug"]},
+{"name": "Pap-Smear Tests in Toronto", "link": "https://www.yorku.ca/science/mathstats/acadic/pap-smear-tests-in-toronto/", "keywords": ["women", "tests", "screen", "pap", "smear", "papsmear", "papanicolaou", "cervix", "cervical", "cancer"]},
+
+
+];
