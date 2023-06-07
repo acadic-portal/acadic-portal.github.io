d19e90968ad576d4cafd13f022ce9e1eb6171ce4
acadic-portal <122941412+acadic-portal@users.noreply.github.com>
2023-06-07 10:51:03 -0400

    Add files via upload

---
+++ Jane&Finch/ACADICSearch.js
@@ -0,0 +1,13 @@
+searchkeywords = [
+{"name": "All Urgency Emergency Department Visits in Toronto", "link": "https://acadic.org/all-emergency-department-visits-in-toronto/", "keywords": "emergency department ed urgent all urgency"},
+{"name": "Asthma in Toronto", "link": "https://acadic.org/asthma-in-toronto/", "keywords": "preventive disease asthma respiratory breath genetic lung corticosteroids"},
+{"name": "COPD in Toronto", "link": "https://acadic.org/copd-in-toronto/", "keywords": "preventive disease copd chronic obstructive pulmonary smoke respiratory cough breath lung"},
+{"name": "Diabetes in Toronto", "link": "https://acadic.org/diabetes-in-toronto/", "keywords": "preventive disease chronic diabetes insulin blood sugar beta"},
+{"name": "High-Urgency Emergency Department Visits in Toronto", "link": "https://acadic.org/high-urgency-emergency-department-visits-in-toronto/", "keywords": "emergency department ed urgent high urgency"},
+{"name": "Hypertension in Toronto", "link": "https://acadic.org/hypertension-in-toronto/", "keywords": "preventive disease hypertension hbp high blood pressure genetic smoke alcohol"},
+{"name": "Low-Urgency Emergency Department Visits in Toronto", "link": "https://acadic.org/low-urgency-emergency-department-visits-in-toronto/", "keywords": "emergency department ed urgent low urgency"},
+{"name": "Mammogram Tests in Toronto", "link": "https://acadic.org/mammogram-tests-in-toronto/", "keywords": "women tests screen mammogram mammography breast cancer"},
+{"name": "Mental Health in Toronto", "link": "https://acadic.org/mental-health-in-toronto/", "keywords": "preventive disease mental health disorder depression drug"},
+{"name": "Pap-Smear Tests in Toronto", "link": "https://acadic.org/pap-smear-tests-in-toronto/", "keywords": "women tests screen pap smear papsmear papanicolaou cervix cervical cancer"},
+
+];
