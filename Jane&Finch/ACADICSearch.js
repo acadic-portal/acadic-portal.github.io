a7f908ae6a0fc5878a53e74d24d24193f52139f3
acadic-portal <122941412+acadic-portal@users.noreply.github.com>
2023-07-13 19:52:57 -0400

    Update ACADICSearch.js

--- Jane&Finch/ACADICSearch.js
+++ Jane&Finch/ACADICSearch.js
@@ -1,13 +1,13 @@
 searchkeywords = [
-{"name": "All Urgency Emergency Department Visits in Toronto", "link": "https://acadic.org/all-emergency-department-visits-in-toronto/", "keywords": "emergency department ed urgent all urgency"},
-{"name": "Asthma in Toronto", "link": "https://acadic.org/asthma-in-toronto/", "keywords": "preventive disease asthma respiratory breath genetic lung corticosteroids"},
-{"name": "COPD in Toronto", "link": "https://acadic.org/copd-in-toronto/", "keywords": "preventive disease copd chronic obstructive pulmonary smoke respiratory cough breath lung"},
-{"name": "Diabetes in Toronto", "link": "https://acadic.org/diabetes-in-toronto/", "keywords": "preventive disease chronic diabetes insulin blood sugar beta"},
-{"name": "High-Urgency Emergency Department Visits in Toronto", "link": "https://acadic.org/high-urgency-emergency-department-visits-in-toronto/", "keywords": "emergency department ed urgent high urgency"},
-{"name": "Hypertension in Toronto", "link": "https://acadic.org/hypertension-in-toronto/", "keywords": "preventive disease hypertension hbp high blood pressure genetic smoke alcohol"},
-{"name": "Low-Urgency Emergency Department Visits in Toronto", "link": "https://acadic.org/low-urgency-emergency-department-visits-in-toronto/", "keywords": "emergency department ed urgent low urgency"},
-{"name": "Mammogram Tests in Toronto", "link": "https://acadic.org/mammogram-tests-in-toronto/", "keywords": "women tests screen mammogram mammography breast cancer"},
-{"name": "Mental Health in Toronto", "link": "https://acadic.org/mental-health-in-toronto/", "keywords": "preventive disease mental health disorder depression drug"},
-{"name": "Pap-Smear Tests in Toronto", "link": "https://acadic.org/pap-smear-tests-in-toronto/", "keywords": "women tests screen pap smear papsmear papanicolaou cervix cervical cancer"},
+{"name": "All Urgency Emergency Department Visits in Northwest Toronto", "link": "https://acadic.org/all-emergency-department-visits-in-northwest-toronto/", "keywords": "emergency department ed urgent all urgency in northwest toronto"},
+{"name": "Asthma in Northwest Toronto", "link": "https://acadic.org/asthma-in-northwest-toronto/", "keywords": "preventive disease asthma respiratory breath genetic lung corticosteroids in northwest toronto"},
+{"name": "COPD in Northwest Toronto", "link": "https://acadic.org/copd-in-northwest-toronto/", "keywords": "preventive disease copd chronic cronic obstructive pulmonary smoke respiratory cough breath lung in northwest toronto"},
+{"name": "Diabetes in Northwest Toronto", "link": "https://acadic.org/diabetes-in-northwest-toronto/", "keywords": "preventive disease chronic cronic diabetes insulin blood sugar beta in northwest toronto"},
+{"name": "High-Urgency Emergency Department Visits in Northwest Toronto", "link": "https://acadic.org/high-urgency-emergency-department-visits-in-northwest-toronto/", "keywords": "emergency department ed urgent high urgency in northwest toronto"},
+{"name": "Hypertension in Northwest Toronto", "link": "https://acadic.org/hypertension-in-northwest-toronto/", "keywords": "preventive disease hypertension hbp high blood pressure genetic smoke alcohol in northwest toronto"},
+{"name": "Low-Urgency Emergency Department Visits in Northwest Toronto", "link": "https://acadic.org/low-urgency-emergency-department-visits-in-northwest-toronto/", "keywords": "emergency department ed urgent low urgency in northwest toronto"},
+{"name": "Mammogram Tests in Northwest Toronto", "link": "https://acadic.org/mammogram-tests-in-northwest-toronto/", "keywords": "women tests screen mammogram mammography breast cancer in northwest toronto"},
+{"name": "Mental Health in Northwest Toronto", "link": "https://acadic.org/mental-health-in-northwest-toronto/", "keywords": "preventive disease mental health disorder depression drug in northwest toronto"},
+{"name": "Pap-Smear Tests in Northwest Toronto", "link": "https://acadic.org/pap-smear-tests-in-northwest-toronto/", "keywords": "women tests screen pap smear papsmear papanicolaou cervix cervical cancer in northwest toronto"},
 
 ];
