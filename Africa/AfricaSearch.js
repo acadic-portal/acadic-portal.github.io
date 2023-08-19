1bbae5b1e26d8b0ede0c05c906ef01314276f251
acadic-portal <122941412+acadic-portal@users.noreply.github.com>
2023-08-19 11:19:14 -0400

    Update AfricaSearch.js

--- Africa/AfricaSearch.js
+++ Africa/AfricaSearch.js
@@ -5,7 +5,8 @@ africasearch = [
   {"name":"Malaria in Ghana","link":"https://acadic.org/malaria-in-ghana/","keywords":"malaria in ghana vector mosquito anopheles"},
   {"name":"Dengue Fever in Africa","link":"https://acadic.org/dengue-fever-in-africa/","keywords":"dengue yellow fever in africa vector mosquito aedes"},
   {"name":"Dengue Fever in Africa","link":"https://acadic.org/dengue-fever-in-africa/","keywords":"dengue yellow fever in africa vector mosquito aedes"},
-  {"name":"COVID-19 Dashboards","link":"https://acadic.org/covid-19-dashboards/","keywords":"covid-19 covid19 coronavirus raspitory sarscov vax vaccine vaccination"},{"name":"COVID-19 Sentiment Dashboard","link":"https://acadic.org/covid-19-sentiment-dashboard/","keywords":"covid-19 covid19 coronavirus raspitory sarscov sentiment social media vax vaccine vaccination"},
+  {"name":"COVID-19 Dashboards","link":"https://acadic.org/covid-19-dashboards/","keywords":"covid-19 covid19 coronavirus respiratory sarscov vax vaccine vaccination"},
+  {"name":"COVID-19 Sentiment Dashboard","link":"https://acadic.org/covid-19-sentiment-dashboard/","keywords":"covid-19 covid19 coronavirus respiratory sarscov sentiment social media vax vaccine vaccination"},
   {"name":"Mpox (Monkeypox) in Africa","link":"https://acadic.org/mpox-in-africa/","keywords":"mpox monkeypox sexually transmitted in africa poxvirus"},
   {"name":"rotavirus in Africa","link":"https://acadic.org/rotavirus-in-africa/","keywords":"rotavirus in africa diarrhoea vaccine vaccination"}
 ];
